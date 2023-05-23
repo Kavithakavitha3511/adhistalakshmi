d5724d544467281f3a7739d1dc1f2705aa584d5d
Kavithakavitha3511 <133203408+Kavithakavitha3511@users.noreply.github.com>
2023-05-23 09:39:16 +0530

    Add files via upload

---
+++ trianglepattern.py
@@ -0,0 +1,71 @@
+## ****
+## ***
+## **
+## *
+##
+## 
+##rows=int(input("enter the row value"))
+##for i in range(rows+1,0,-1):
+##    for j in range(1,i):
+##        print("*", end=" ")
+##    print()
+
+
+##
+##*
+##* *
+##* * *
+##rows=int(input("enter the row value"))
+##for i in range(1,rows+1):
+##    for j in range(1,i+1):
+##        print("*", end=" ")
+##    print()
+
+
+
+##* * *
+##  * *
+##    *
+
+##rows=int(input("enter the value row value"))
+##for i in range(rows):
+##    for j in range(i):
+##        print(" ",end=" ")
+##    for j in range(rows,i,-1):
+##        print("*", end=" ")
+##    print()
+
+##
+##    *
+##  * *
+##* * *
+##
+##
+##rows=int(input("enter the value row value"))
+##for i in range(rows):
+##    for j in range(1,rows-i):
+##        print(" ",end=" ")
+##    for j in range(0,i+1):
+##        print("*", end=" ")
+##    print()
+##
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
