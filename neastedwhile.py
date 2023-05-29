b62401b5d2c4a849b3345409efee66f6f1e683ab
Kavithakavitha3511 <133203408+Kavithakavitha3511@users.noreply.github.com>
2023-05-29 13:32:11 +0530

    Add files via upload

---
+++ neastedwhile.py
@@ -0,0 +1,48 @@
+##i=1
+##while(i<=5):
+##    print("ocean", end=" ")
+##    j=1
+##    while(j<=1):
+##        print("academy", end=" ")
+##        j=j+1
+##    print()
+##    i=i+1
+
+
+##i=0
+##while(i<=5):
+##    print("*", end=" ")
+##    j=1
+##    while(j<=i):
+##        print("*", end=" ")
+##        j=j+1
+##    print()
+##    i=i+1
+
+
+i=0
+while(i<=5):
+    print("*", end=" ")
+    j=1
+    while(j<=i):
+        print("*", end=" ")
+        j=j+1
+    print()
+    i=i+1
+i=0
+while(i<=5):
+    print("*", end=" ")
+    i=i+1
+    j=1
+    while(j<=5):
+        print("*", end=" ")
+        j=j+1
+    print()
+    
+
+
+
+
+    
+    
+
