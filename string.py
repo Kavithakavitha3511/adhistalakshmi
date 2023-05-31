28932629597a8cdfdf785d92714cc9ddcbdb3ebc
Kavithakavitha3511 <133203408+Kavithakavitha3511@users.noreply.github.com>
2023-05-31 13:35:32 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,18 @@
+name=(input("enter the nmae"))
+print("hello"+" "+name+"!")
+
+
+a=(input("enter a:"))
+b=(input("enter b:"))
+print(a+b+b+a)
+
+
+a=(input("enter a:"))
+b=(input("enter b:"))
+print("<"+a+">"+b+"<"+"/"+a+">")
+
+
+a=(input("enter a:"))
+b=(input("enter b:"))
+print(a[0:2]+b+a[2:4])
+
