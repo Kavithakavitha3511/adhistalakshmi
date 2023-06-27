c5226e569b500a923e6ee3ecda65be78954be5a0
Kavithakavitha3511 <133203408+Kavithakavitha3511@users.noreply.github.com>
2023-06-27 14:45:22 +0530

    Update argumantpassing.py

--- argumantpassing.py
+++ argumantpassing.py
@@ -18,4 +18,33 @@ add(b=5,a=6)
 def add(a,b=6):
     print(a+b)
 add(8)
-    
+
+##EXAMPLE 
+def swap():
+    a=10
+    b=6
+    a,b=b,a
+    print("a is swapping", a)
+    print("b is swapping", b)
+swap()
+
+def swap(a,b):
+    a,b=b,a
+    print("a is swapping", a)
+    print("b is swapping", b)
+swap(10,6)
+
+
+def swap(a,b):
+    a,b=b,a
+    print("a is swapping", a)
+    print("b is swapping", b)
+swap(a=20,b=10)
+
+
+def swap(a,b=10):
+    a,b=b,a
+    print("a is swapping", a)
+    print("b is swapping", b)
+swap(60)
+
