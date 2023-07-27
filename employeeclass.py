4da5ba030b61a6a7c0c8b7405e3b52801d0a5e32
Kavithakavitha3511 <133203408+Kavithakavitha3511@users.noreply.github.com>
2023-07-27 02:47:12 -0700

    Add files via upload

---
+++ employeeclass.py
@@ -0,0 +1,35 @@
+class Employee:
+    name="Adhistalakshmi"
+    e_no=9240
+    designation="accountant"
+    salary=25000
+    address="pondicherry"
+    email="adhist@2000gmail.com"
+    mobile_no=6598741235
+    def details(self):
+        print("name:\t\t",self.name)
+        print("e_no:\t\t",self.e_no)
+        print("designation:\t",self.designation)
+        print("salary:\t\t",self.salary)
+        print("address:\t",self.address)
+        print("email:\t\t",self.email)
+        print("mobile_no:\t",self.mobile_no)
+obj=Employee()
+obj.details()
+
+
+##
+##class Student:
+##    name="vidhya"
+##    age=20
+##    def print(self):
+##        print("hello world")
+##        print("name: ", self.name)
+##obj=Student()
+##obj.print()
+##print(obj.name)
+##print(obj.age)
+
+
+        
+    
